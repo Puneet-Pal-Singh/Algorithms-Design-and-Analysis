b9f4d1de3d4bbe0039f9b30cca87215910ee6686
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-10-10 23:13:58 +0530

    Update Merge Sort.cpp

--- Practical Examples/Sorting and Searching/Merge Sort.cpp
+++ Practical Examples/Sorting and Searching/Merge Sort.cpp
@@ -62,12 +62,53 @@ void mergeSort(int array[], int l, int r)
              merge(array, l, m, r);
    }
 }
+int co(int i,int arr[5000],int n)
+{ int ch;
+   switch (ch)
+   {
+   case 1: for ( i = 0; i < n; i++)
+           arr[i]=i;
+           mergeSort(arr, 0, n-1);   
+           break;
+   case 2: for ( i = 0; i < n; i++)
+           arr[i]=n-i;
+           mergeSort(arr, 0, n-1);   
+           break;
+   case 3: 
+           for ( i = 0; i < n; i++)
+           arr[i]=rand();
+           mergeSort(arr, 0, n-1);   
+           break;        
+   default:cout<<"Invalid choice:\n";
+      break;
+      exit(0);
+   }
+return 0;
+}
+
+
+
+
 int main() 
 {
-   int n;
+   int n,i;
+   clock_t start,end;
+   float time;
+   
+   
    cout << "Enter the number of elements: ";
    cin >> n;
    int arr[n];     //create an array with given number of elements
+   
+   // Displaying Cases
+   int ch;
+   cout<<"Choose Case \n 1. Best Case: \n 2. Average Case: \n 3. Worst Case: \n";
+   cin>>ch;
+   co(i,arr,n);
+   start = clock();
+
+
+   // Dispalying Array
    cout << "Enter elements:" << endl;
    for(int i = 0; i<n; i++) 
    {
@@ -78,4 +119,9 @@ int main()
    mergeSort(arr, 0, n-1);     //(n-1) for last index
    cout << "Array after Sorting: ";
    display(arr, n);
+
+   // Displaying time
+   end=clock();
+   time = (end-start)/CLK_TCK;
+   cout<<"Time taken by sorting is:"<<time;
 }
